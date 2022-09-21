41745c44964fb9e22b65fc27a8db36c100dd749d
刘伟 <lemonpaimc@126.com>
2022-09-21 21:40:07 +0800

    test(test): test

--- packages/web/src/layouts/main.tsx
+++ packages/web/src/layouts/main.tsx
@@ -8,7 +8,7 @@ export default function Layout() {
     const { update } = useContext(ThemeContent);
     return (
         <Application>
-            <AppBar fixed><div>自动change3</div></AppBar>
+            <AppBar fixed><div>自动change4</div></AppBar>
             <Outlet />
         </Application>
     );
